94c68e3674b60787269ee439e8fffb2f1a3d007c
DAYFA1R <im@dayfair.ca>
2025-05-11 17:12:56 -0400

    Resolving some issues related to accessing optional fields

--- src/command/command.cpp
+++ src/command/command.cpp
@@ -2,14 +2,18 @@
 
 // Lets you pass in a reference to a string, and allows you to get a subcommand by
 const Command* Command::findSubcommand(const std::string& argument) const {
+  // if this command doesn't have any subcommands
+  if (!subcommands.has_value()) {
+    return nullptr;
+  }
   // looking through each subcommand
-  for (const Command& subcommand : subcommands) {
+  for (const Command& subcommand : subcommands.value()) {
     // matching by the subcommand name OR argument value
-    if (subcommand.argument == argument || subcommand.argument == argument) {
+    if (subcommand.name == argument || subcommand.argument == argument) {
       // returning a reference to the subcommand if its found
       return &subcommand;
     }
-    // or a nullptr if not
-    return nullptr;
   }
+  // or a nullptr if not
+  return nullptr;
 }
--- src/command/command.h
+++ src/command/command.h
@@ -2,16 +2,15 @@
 
 #include <string>
 #include <vector>
-#include <memory>
+#include <optional>
 
 struct Command {
   std::string name; // this might be superfluous, but adding it for now
   std::string argument; // shorthand cmd (eg. nyd <argument>)
-  std::string cd; // where does it execute
-  std::string exec; // what does it execute
-  std::string pre; // pre-exection status
-  std::vector<Command> subcommands; // reference to children, default to empty vector if no descendents
+  std::optional<std::string> cd; // where does it execute
+  std::optional<std::string> exec; // what does it execute
+  std::optional<std::string> pre; // pre-exection status
+  std::optional<std::vector<Command>> subcommands; // reference to children, default to empty vector if no descendents
 
   const Command* findSubcommand(const std::string& arg) const; // use the command-line arg to fetch correct sub-command object
 };
-
