618556de3664c4a5deaf1381b67dca7a994d5399
DAYFA1R <im@dayfair.ca>
2025-05-11 15:41:50 -0400

    laying out some steps for impl in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include <yaml-cpp/yaml.h>
 
 int main() {
+    // Find and load YAML
+
+    // Traverse YAML and create Segment tree
+
+    // Parse args and map to Segments
+    
+
+    //placeholder to test YAML lib
     YAML::Node config = YAML::Load("{name: Conan, type: Package}");
     std::cout << "Name: " << config["name"].as<std::string>() << "\n";
     std::cout << "blep \n";
