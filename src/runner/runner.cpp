9a4f77594dc67f2346c16cd79dc361f6797da36c
DAYFA1R <im@dayfair.ca>
2025-05-12 00:43:58 -0400

    Adding sweet terminal highlighting

--- src/runner/runner.cpp
+++ src/runner/runner.cpp
@@ -4,6 +4,9 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "../utils/path.h"
+#include "../utils/colors.h"
+
 namespace runner {
   void executeCommand(const Command* command) {
     if (!command) {
@@ -14,9 +17,10 @@ namespace runner {
     // Change to the current working directory if cd is provided and non-empty
     if (command->cd && !command->cd->empty()) {
       try {
-        std::filesystem::path path = *command->cd; // dereference command, so we can access cd (command->cd), then deference the optional (*(command->cd)), so we can access the string!
+        // deref command, so we can access cd (command->cd), then defer the Optional (*(command->cd)), so we can access the string!
+        std::filesystem::path path = utils::expandTilde(*command->cd);
         std::filesystem::current_path(path); // set cd to current path
-        std::cout << "→ Switched to directory: " << std::filesystem::current_path() << "\n"; // log dir change
+        std::cout << CLR_GOLD << "→ " << CLR_PURPLE << "Burrowing to: " << CLR_RESET << std::filesystem::current_path() << "\n"; // log dir change
       } catch (const std::filesystem::filesystem_error& error) {
         std::cerr << "Failed to change directory: " << error.what() << "\n"; // log error
         return;
@@ -25,13 +29,13 @@ namespace runner {
   
     // Run the prefix command if its there
     if (command->pre && !command->pre->empty()) {
-      std::cout << "→ Running pre-command: " << *command->pre << "\n";
+      std::cout << CLR_GOLD << "→ " << CLR_PURPLE << "Moving Overlords: " << CLR_RESET << *command->pre << "\n";
       std::system(command->pre->c_str());
     }
   
     // Run the main command in exec
     if (command->exec && !command->exec->empty()) {
-      std::cout << "→ Running command: " << *command->exec << "\n";
+      std::cout << CLR_GOLD << "↳ " << CLR_PINK << "Executing Hivemind directive: " << CLR_RESET << *command->exec << "\n";
       std::system(command->exec->c_str());
     } else {
       std::cout << "No command to execute.\n";
