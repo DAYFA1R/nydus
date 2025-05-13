f00a5d1bb33262dcd2a589a67d25a991cdf74e4b
DAYFA1R <im@dayfair.ca>
2025-05-12 22:19:52 -0400

    Hello codecov where are you

--- src/command/command.cpp
+++ src/command/command.cpp
@@ -8,12 +8,12 @@ const Command* resolveCommand(const std::vector<Command>& commands, const std::v
   const std::vector<Command>* currentLevel = &commands; // set the currentLevel as the parsed command vector
   
   // for each argument provided in the CLI
-  for (const auto& argument : arguments) {
+  for (const std::string& argument : arguments) {
     bool matched = false; // set default state (because we haven't found anything yet)
     // look at the current command
-    for (const auto& command : *currentLevel) {
+    for (const Command& command : *currentLevel) {
       // if the command is this one
-      if (command.name == argument || command.argument == argument) {
+      if (command.name == argument || command.argument == argument) {https://github.com/DAYFA1R/nydus/tree/main/.github/workflows
         currentCommand = &command; // set the current command to this one
         matched = true; // because we totally found it!
         // now, if there are child commands
