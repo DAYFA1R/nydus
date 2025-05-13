de48fd48d9a8ce874712fa7f6bbc64ca81ffc5d3
Daniel Hicks <im@dayfair.ca>
2025-05-12 21:58:01 -0400

    Moving auto back to specific type to trigger codecov diff (#2)

--- src/command/command.cpp
+++ src/command/command.cpp
@@ -2,13 +2,12 @@
 
 #include <iostream>
 
-// Matches args to an actual command in the tree
 const Command* resolveCommand(const std::vector<Command>& commands, const std::vector<std::string>& arguments) {
   const Command* currentCommand = nullptr; // default to no command found
   const std::vector<Command>* currentLevel = &commands; // set the currentLevel as the parsed command vector
   
   // for each argument provided in the CLI
-  for (const auto& argument : arguments) {
+  for (const std::string& argument : arguments) {
     bool matched = false; // set default state (because we haven't found anything yet)
     // look at the current command
     for (const Command& command : *currentLevel) {
